afea2a74f15c684209981e066368a8b3e25c9c34
Dmitry Moiseev <149093616+Create2702@users.noreply.github.com>
2026-05-27 16:37:10 +0400

    feat: Add bulk shear display

--- src/table.py
+++ src/table.py
@@ -13,7 +13,7 @@ def check_for_round(param):
     else:
         return round(param)
 
-def open_table(cape, cin, lcl, lfc, el, srh_01, srh_03, srh_06, year, month, day, hour, station) -> None:
+def open_table(cape, cin, lcl, lfc, el, srh_01, srh_03, srh_06, blk_shr_06, year, month, day, hour, station) -> None:
     table = plt.figure(figsize=(10, 10))
     table.canvas.manager.set_window_title(f'TSLHDPy v0.1.0-alpha ({year}-{month}-{day} {hour} UTC. Station number: {station})')
     ax = table.add_subplot(111)
@@ -28,6 +28,7 @@ def open_table(cape, cin, lcl, lfc, el, srh_01, srh_03, srh_06, year, month, day
         f'SRH 0-1 km: {check_for_round(check(srh_01.m))} $m^2/s^2$\n'
         f'SRH 0-3 km: {check_for_round(check(srh_03.m))} $m^2/s^2$\n'
         f'SRH 0-6 km: {check_for_round(check(srh_06.m))} $m^2/s^2$\n'
+        f'Bulk Shear 0-6 km:  {check_for_round(check(blk_shr_06.m))} knots\n'
     )
     ax.text(0.05, 0.95, text, fontsize=16, fontfamily='monospace', va='top')
     
