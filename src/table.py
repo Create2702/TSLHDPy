a1a8cbbc92b6066006c8277804dc016700cad3ad
Dmitry Moiseev <149093616+Create2702@users.noreply.github.com>
2026-05-27 12:06:13 +0400

    feat: Add SRH display to the table

--- src/table.py
+++ src/table.py
@@ -13,14 +13,21 @@ def check_for_round(param):
     else:
         return round(param)
 
-def open_table(cape, cin, lcl, lfc, el, year, month, day, hour, station) -> None:
+def open_table(cape, cin, lcl, lfc, el, srh_01, srh_03, srh_06, year, month, day, hour, station) -> None:
     table = plt.figure(figsize=(10, 10))
     table.canvas.manager.set_window_title(f'TSLHDPy v0.1.0-alpha ({year}-{month}-{day} {hour} UTC. Station number: {station})')
     ax = table.add_subplot(111)
     ax.axis('off')
-    text = f'CAPE: {check_for_round(check(cape.m))} J/kg\nCIN: {check_for_round(check(cin.m))} J/kg\nLCL: {check_for_round(check(lcl.m))} hPa\nLFC: {check_for_round(check(lfc.m))} hPa\nEL: {check_for_round(check(el.m))} hPa'
-    ax.text(0.05, 0.95, text, fontsize=16)
-    plt.show()
-    plt.close(table)
-
-
+    
+    text = (
+        f'CAPE: {check_for_round(check(cape.m))} J/kg\n'
+        f'CIN: {check_for_round(check(cin.m))} J/kg\n'
+        f'LCL: {check_for_round(check(lcl.m))} hPa\n'
+        f'LFC: {check_for_round(check(lfc.m))} hPa\n'
+        f'EL: {check_for_round(check(el.m))} hPa\n'
+        f'SRH 0-1 km: {check_for_round(check(srh_01.m))} $m^2/s^2$\n'
+        f'SRH 0-3 km: {check_for_round(check(srh_03.m))} $m^2/s^2$\n'
+        f'SRH 0-6 km: {check_for_round(check(srh_06.m))} $m^2/s^2$\n'
+    )
+    ax.text(0.05, 0.95, text, fontsize=16, fontfamily='monospace', va='top')
+    
